58dc70095d0593354281d72a801c73fd1af2b9b5
GeinozG <Gregoire.geinoz@studentfr.ch>
2022-03-29 19:57:46 +0000

    update

--- source/tmconfig.py
+++ source/tmconfig.py
@@ -4,7 +4,7 @@ class TMConfig:
     first_name = 'Grégoire'
     last_name = 'Geinoz'
     author = f'{first_name} {last_name}'
-    year = u'2021'
+    year = u'2022'
     month = u'Décembre'
     seminary_title = u'Développement d’outils ou matériel d’enseignement de l’informatique'
     tutor = u"Cédric Donner"
